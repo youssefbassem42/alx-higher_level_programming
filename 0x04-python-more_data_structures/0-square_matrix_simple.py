0bdf5a492ac15edc16b47bf0ea2dbdac8865363f
youssefbassem42 <youssefbassem42@gmail.com>
2023-11-09 01:16:30 +0200

    a

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]) :
+def square_matrix_simple(matrix=None) :
     """function get matrix squares"""
+    if matrix is None :
+        matrix = []
     return [list(map(lambda x: x ** 2,row)) for row in matrix]
