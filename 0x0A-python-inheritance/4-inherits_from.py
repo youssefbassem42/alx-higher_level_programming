b22319b4fe5ec5fb4fb42295af2177cdf8ebfff8
youssefbassem42 <youssefbassem42@gmail.com>
2023-12-04 21:43:24 +0200

    delete main test

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -12,10 +12,3 @@ def inherits_from(obj, a_class):
         return True
     else:
         return False
-a = True
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
-if inherits_from(a, bool):
-    print("{} inherited from class {}".format(a, bool.__name__))
-if inherits_from(a, object):
-    print("{} inherited from class {}".format(a, object.__name__))
