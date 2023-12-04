cf9e5fa0e5e1f039e9eb7c27a0b94435e1fb9f70
youssefbassem42 <youssefbassem42@gmail.com>
2023-12-04 21:17:20 +0200

    subclass

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """" is direct of not """
-    return (isinstance(obj, a_class) and type(obj) != a_class)
+    return (issubclass(obj, a_class) and type(obj) != a_class)
