1b6f9328bbac3eb93e046d73467afdfce625d0d2
youssefbassem42 <youssefbassem42@gmail.com>
2023-12-04 21:43:01 +0200

    script error

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/bin/python3
 """checks if object is an instance of a class that
 inherited from the specified class or not
 """
@@ -12,3 +12,10 @@ def inherits_from(obj, a_class):
         return True
     else:
         return False
+a = True
+if inherits_from(a, int):
+    print("{} inherited from class {}".format(a, int.__name__))
+if inherits_from(a, bool):
+    print("{} inherited from class {}".format(a, bool.__name__))
+if inherits_from(a, object):
+    print("{} inherited from class {}".format(a, object.__name__))
