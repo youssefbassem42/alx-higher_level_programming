ee52a6105f2ce9e63c35aa9a39b4165a6e7da8d1
youssefbassem42 <62960010+youssefbassem42@users.noreply.github.com>
2024-02-18 22:27:07 +0200

    Update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,9 +14,10 @@ if __name__ == '__main__':
 
     db_cursor = db_connect.cursor()
 
-    db_cursor.execute("SELECT * FROM states")
+    db_cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
     rows_selected = db_cursor.fetchall()
 
     for row in rows_selected:
         print(row)
+    db_connect.close()
