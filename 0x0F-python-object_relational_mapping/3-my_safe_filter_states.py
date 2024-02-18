1792e5d5354eab1da28304c1121e8367b189a812
youssefbassem42 <62960010+youssefbassem42@users.noreply.github.com>
2024-02-19 01:24:29 +0200

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
-""" """
+"""
+script that takes in arguments and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+But this time, write one that is safe from MySQL injections!
+"""
+
 
 from sys import argv
 import MySQLdb
 
+
 if __name__ == "__main__":
+    """
+        Access to the database and get the states
+        from the database.
+    """
     connector = MySQLdb.Connect(
         host="localhost",
         port=3306,
