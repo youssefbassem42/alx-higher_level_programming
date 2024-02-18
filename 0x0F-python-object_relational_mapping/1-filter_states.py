80954aef46223b3bab562873568e891ab66f79a4
youssefbassem42 <62960010+youssefbassem42@users.noreply.github.com>
2024-02-19 00:11:03 +0200

    Update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,11 +7,11 @@ import MySQLdb
 
 if __name__ == '__main__':
     connector = MySQLdb.Connect(
-        host = "localhost",
-        port = 3306,
-        user = argv[1],
-        passwd = argv[2],
-        db = argv[3])
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
     cursor = connector.cursor()
     cursor.execute("""SELECT * FROM states WHERE states.name LIKE\
                    BINARY "N%" ORDER BY states.id ASC """)
